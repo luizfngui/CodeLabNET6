18ab0380f8c3ec324ce0eefdda92c402e12ed4a1
Alexandre <alexandre.vieira@athosnegocios.com>
2023-07-13 00:48:30 -0300

    first commit

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,32 @@
+﻿using CodeLabNET6.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeLabNET6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+        [HttpPost]
+        public IActionResult AdicionaFilme([FromBody] Movie movie)
+        {
+            movie.Id = Guid.NewGuid();
+            movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovieById), new { id = movie.Id });
+        }
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies([FromQuery] int skip = 0, [FromQuery] int take = 10 )
+        {
+            return movies.Skip(skip).Take(take);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieById(Guid id)
+        {
+            var movie = movies.FirstOrDefault(movie => movie.Id == id);
+            if (movie == null) return NotFound();
+            return Ok(movie);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CodeLabNET6.Models
+{
+    public class Movie
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "The title is required")]
+        [MaxLength(50, ErrorMessage = "Title has a maximum of 50 characteres")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "The genre is required")]
+        [MaxLength(50, ErrorMessage = "Genre has a maximum of 50 characteres")]
+        public string Genre  { get; set; }
+        [Required(ErrorMessage = "The duration is required")]
+        [Range(30,600,ErrorMessage ="Duration must be between 30 minutes and 600 minutes")]
+        public int Duration { get; set; }
+    }
+}
